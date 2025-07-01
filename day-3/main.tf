21b002727e2a2908f48d4ec71dbb81e73529ca5c
Derakings <chideraojimba878@gmail.com>
2025-07-02 00:34:31 +0100

    Task completed

---
+++ day-3/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ubuntu_server" {
+  ami           = "ami-0fc5d935ebf8bc3bc" 
+  instance_type = "t2.micro"
+
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install nginx -y
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
+  tags = {
+    Name = "Ubuntu-Web-Server"
+  }
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+output "public_ip" {
+  value = aws_instance.ubuntu_server.public_ip
+}
